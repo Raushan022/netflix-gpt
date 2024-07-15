75bd7310ef509995421ade2ae1ec184996c79399
Raushan Kumar <raushan.kumar@p360.com>
2024-07-15 07:25:38 -0400

    login form two

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -16,16 +16,27 @@ const Login = () => {
 
       <form className='w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
         <h1 className='font-bold text-3xl py-4'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
+        {!isSignInForm && <input
+          type="text"
+          placeholder='Full Name'
+          className='p-3 my-3 w-full bg-gray-700 rounded-sm'
+        />}
         <input
           type="text"
           placeholder='Email Address'
-          className='p-3 my-3 w-full' />
+          className='p-3 my-3 w-full bg-gray-700 rounded-sm'
+        />
         <input
           type="password"
           placeholder='Password'
-          className='p-3 my-3 w-full' />
-        <button className='p-2 my-4 bg-red-600 w-full rounded-lg'>{isSignInForm ? "Sign In" : "Sign Up"}</button>
-        <p className='py-4' onClick={toggleSignInForm}>New to Netflix? Sign up Now</p>
+          className='p-3 my-3 w-full bg-gray-700 rounded-sm'
+        />
+        <button className='p-2 my-4 bg-red-600 w-full rounded-sm'>
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className='py-4 cursor-pointer' onClick={toggleSignInForm}>
+          {isSignInForm ? "New to Netflix? Sign up" : "Already registered? Sign In Now"}
+        </p>
 
       </form>
     </div>
